3558dcc26abbdcd5dcab3a388dac707f3ff4c3c1
isaac <isaac@forensiq.com>
2016-01-06 17:53:16 -0500

    oh well prolly doesn't work with windows now

--- screenCapture.js
+++ screenCapture.js
@@ -3,11 +3,14 @@ var page = webPage.create();
 var pokemon = require("system").args[1];
 var url = 'http://www.' + pokemon + '.com';
 
-
-
 page.viewportSize = { width: 1920, height: 1080 };
+
 page.open(url, function start(status) {
-  page.render(pokemon + '.jpeg', {format: 'jpeg', quality: '100'});
-  console.log('captured:', url);
-  phantom.exit();
+
+	page.render('images/' + pokemon + '.jpeg', {format: 'jpeg', quality: '100'});
+
+	console.log('captured:', url);
+
+	phantom.exit();
+
 });
