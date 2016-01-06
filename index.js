771eb3cdf921080ef7f498d4b882e9b79a185345
isaac <isaac@forensiq.com>
2016-01-06 17:37:35 -0500

    ready to rock

--- index.js
+++ index.js
@@ -2,16 +2,178 @@ var path = require('path');
 var childProcess = require('child_process');
 var phantomjs = require('phantomjs');
 var async = require('async');
- 
-var childArgs = [
-  path.join(__dirname, 'screenCapture.js'),
-  'bulbasaur'
-]
- 
-childProcess.execFile('phantomjs', childArgs, function(err, stdout, stderr) {
-  // handle results
-  if (err) {
-  	throw err;
-  }
-  console.log(stdout);
+
+var pokemonNames = [
+	"Bulbasaur", 
+	"Ivysaur", 
+	"Venusaur", 
+	"Charmander", 
+	"Charmeleon", 
+	"Charizard", 
+	"Squirtle", 
+	"Wartortle", 
+	"Blastoise", 
+	"Caterpie", 
+	"Metapod", 
+	"Butterfree", 
+	"Weedle", 
+	"Kakuna", 
+	"Beedrill", 
+	"Pidgey", 
+	"Pidgeotto", 
+	"Pidgeot", 
+	"Rattata", 
+	"Raticate", 
+	"Spearow", 
+	"Fearow", 
+	"Ekans", 
+	"Arbok", 
+	"Pikachu", 
+	"Raichu", 
+	"Sandshrew", 
+	"Sandslash", 
+	"Nidoran♀", 
+	"Nidorina", 
+	"Nidoqueen", 
+	"Nidoran♂", 
+	"Nidorino", 
+	"Nidoking", 
+	"Clefairy", 
+	"Clefable", 
+	"Vulpix", 
+	"Ninetales", 
+	"Jigglypuff", 
+	"Wigglytuff", 
+	"Zubat", 
+	"Golbat", 
+	"Oddish", 
+	"Gloom", 
+	"Vileplume", 
+	"Paras", 
+	"Parasect", 
+	"Venonat", 
+	"Venomoth", 
+	"Diglett", 
+	"Dugtrio", 
+	"Meowth", 
+	"Persian", 
+	"Psyduck", 
+	"Golduck", 
+	"Mankey", 
+	"Primeape", 
+	"Growlithe", 
+	"Arcanine", 
+	"Poliwag", 
+	"Poliwhirl", 
+	"Poliwrath", 
+	"Abra", 
+	"Kadabra", 
+	"Alakazam", 
+	"Machop", 
+	"Machoke", 
+	"Machamp", 
+	"Bellsprout", 
+	"Weepinbell", 
+	"Victreebel", 
+	"Tentacool", 
+	"Tentacruel", 
+	"Geodude", 
+	"Graveler", 
+	"Golem", 
+	"Ponyta", 
+	"Rapidash", 
+	"Slowpoke", 
+	"Slowbro", 
+	"Magnemite", 
+	"Magneton", 
+	"Farfetch'd", 
+	"Doduo", 
+	"Dodrio", 
+	"Seel", 
+	"Dewgong", 
+	"Grimer", 
+	"Muk", 
+	"Shellder", 
+	"Cloyster", 
+	"Gastly", 
+	"Haunter", 
+	"Gengar", 
+	"Onix", 
+	"Drowzee", 
+	"Hypno", 
+	"Krabby", 
+	"Kingler", 
+	"Voltorb", 
+	"Electrode", 
+	"Exeggcute", 
+	"Exeggutor", 
+	"Cubone", 
+	"Marowak", 
+	"Hitmonlee", 
+	"Hitmonchan", 
+	"Lickitung", 
+	"Koffing", 
+	"Weezing", 
+	"Rhyhorn", 
+	"Rhydon", 
+	"Chansey", 
+	"Tangela", 
+	"Kangaskhan", 
+	"Horsea", 
+	"Seadra", 
+	"Goldeen", 
+	"Seaking", 
+	"Staryu", 
+	"Starmie", 
+	"Mr.Mime", 
+	"Scyther", 
+	"Jynx", 
+	"Electabuzz", 
+	"Magmar", 
+	"Pinsir", 
+	"Tauros", 
+	"Magikarp", 
+	"Gyarados", 
+	"Lapras", 
+	"Ditto", 
+	"Eevee", 
+	"Vaporeon", 
+	"Jolteon", 
+	"Flareon", 
+	"Porygon", 
+	"Omanyte", 
+	"Omastar", 
+	"Kabuto", 
+	"Kabutops", 
+	"Aerodactyl", 
+	"Snorlax", 
+	"Articuno", 
+	"Zapdos", 
+	"Moltres", 
+	"Dratini", 
+	"Dragonair", 
+	"Dragonite", 
+	"Mewtwo", 
+	"Mew"
+];
+
+async.eachLimit(pokemonNames, 20, function (name, callback) {
+	var childArgs = [
+		path.join(__dirname, 'screenCapture.js'),
+  		name
+	];
+
+	childProcess.execFile('phantomjs', childArgs, function (err, stdout, stderr) {
+  		if (err) {
+  			return callback(err);
+  		}
+
+  		console.log(stdout);
+
+  		callback();
+	
+	});
+
 });
+
+ 
--- screenCapture.js
+++ screenCapture.js
@@ -3,10 +3,11 @@ var page = webPage.create();
 var pokemon = require("system").args[1];
 var url = 'http://www.' + pokemon + '.com';
 
-console.log('capturing ', url);
+
 
 page.viewportSize = { width: 1920, height: 1080 };
 page.open(url, function start(status) {
   page.render(pokemon + '.jpeg', {format: 'jpeg', quality: '100'});
+  console.log('captured:', url);
   phantom.exit();
 });
