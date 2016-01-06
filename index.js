7522e0963170524cf362d134b2b1a03779345d59
isaac <isaac@forensiq.com>
2016-01-06 17:56:00 -0500

    so we have more consistent formatting

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ var async = require('async');
 var pokemonNames = require('./pokemonNames');
 
 async.eachLimit(pokemonNames, 20, function (name, callback) {
+	
 	var childArgs = [
 		path.join(__dirname, 'screenCapture.js'),
   		name
 	];
 
 	childProcess.execFile('phantomjs', childArgs, function (err, stdout) {
+  		
   		if (err) {
   			console.log(err);
   		}
